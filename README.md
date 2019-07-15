23692c8cca1bc0d2f27c055bb5c071df2c8e7188
Zhai Jinyuan <15210720098@fudan.edu.cn>
2019-07-16 01:21:37 +0800

    pygame and stradegy

--- README.md
+++ README.md
@@ -1,18 +1,57 @@
 # Python(pygame) 贪吃蛇
 
-## How to use
-```
-pip install requirements.txt
+# 使用
+
+```shell
+pip3 install requirements.txt
 python3 main-bfs2.py
 ```
 
-## 说明
-0. python3 
-1. main.py 100行代码的贪吃蛇
-2. main_bfs.py 通过BFS自动规划到食物的最短路径
-但是蛇身太长之后很容易找不到路径而死掉
-3. main_bfs2.py [实现贪吃蛇AI](http://mp.weixin.qq.com/s?__biz=MzA5ODUxOTA5Mg==&mid=211204280&idx=1&sn=4589891ff2ddff98058f44f3e9dd942e&scene=24&srcid=0923YmTbhMIczvNdLDgrgPMz#rd)，按照这个思路进行修改。[code here](https://github.com/Hawstein/snake-ai)
-只是做了一些微小的修改，把curses改为pygame
-代码没面向对象，非常丑陋，然而我也没时间改了:)
+# 思路
+
+主要是参考这篇 [实现贪吃蛇AI](http://mp.weixin.qq.com/s?__biz=MzA5ODUxOTA5Mg==&mid=211204280&idx=1&sn=4589891ff2ddff98058f44f3e9dd942e&scene=24&srcid=0923YmTbhMIczvNdLDgrgPMz#rd)，原实现在[这里](https://github.com/Hawstein/snake-ai)，主要的思路是下面这个图
+
+主要的思路是派出一条假蛇去探路，假蛇吃完食物还能活着，真蛇才会去吃（假蛇吃完食物怎么样算能活，这个比较难判断，我的改动也主要在这里，后面会说）
+
+```flow
+st=>start: 开始
+op_tmp_sanke=>operation: 一条虚拟的蛇探路
+cond_food=>condition: 能否到食物
+cond_tmp_snake=>condition: 虚蛇和尾巴之间有通路
+op_shortest=>operation: 走离食物最短的路
+cond_tail=>condition: 蛇和尾巴之间有通路
+op_longest=>operation: 走离蛇尾最长的路
+cond_move=>condition: 有可行的路
+op_dead=>operation: 你死了
+e=>end: 结束
+
+st->cond_food
+cond_food(yes)->op_tmp_sanke->cond_tmp_snake
+cond_food(no)->cond_tail
+cond_tmp_snake(yes)->op_shortest
+cond_tmp_snake(no)->cond_tail
+cond_tail(yes)->op_longest
+cond_tail(no)->cond_move
+cond_move(yes)->op_shortest
+cond_move(no)->op_dead->e
+
+```
+
+# 改动
+
+只是做了一些微小的修改
+
+1. 把curses改为pygame，界面好看多了
+2. 加了没有什么卵用的类
+3. 假蛇吃到食物之后，怎么算和尾巴之间有通路？
+   1. 头和尾相邻算没有通路（原方法），比较保守，走到最后经常会循环起来，不敢吃食
+   2. 如果头尾相邻算成有通路，容易在前期就把自己撞死
+   3. 没有解决这个问题，加了个判断，蛇默认是保守的，但如果长时间没有吃到食物，就变激进。从测试结果看解决了原有的问题<del>，虽然不优雅</del>
+
+# 待改进
+
+1. 这个算法调起来像无底洞，我应该不会再改进这个了，感觉要加逻辑判断的地方很多，应该会有更优雅的实现
+2. 当最后蛇很长的时候，蛇走和食物的最短路径是不合理的，因为走最短路径留下的空隙很可能会被填上食物，还需要绕一大圈才能吃到。应该是到最后，蛇直接一排一排地扫，反而是最快的
+
 
 
--- main.py
+++ main.py
@@ -1,164 +1,359 @@
+# coding: utf-8
+
 import pygame
 import sys
-import random
+from random import randint, seed
 
-# 全局定义
-SCREEN_X = 600
-SCREEN_Y = 600
+seed(1)
+
+class SnakeConf(object):
+    # 蛇运动的场地长宽
+    HEIGHT, WIDTH = 20, 20
+    LINE_WIDTH = 25
+    LINE_MARGIN = 4
+    LINE_TRUEWIDTH = LINE_WIDTH - 2 * LINE_MARGIN
+    SCREEN_X, SCREEN_Y = LINE_WIDTH * HEIGHT, LINE_WIDTH * WIDTH
+    FIELD_SIZE = HEIGHT * WIDTH
+
+    # 用来代表不同东西的数字，由于矩阵上每个格子会处理成到达食物的路径长度，
+    # 因此这三个变量间需要有足够大的间隔(>HEIGHT*WIDTH)
+    FOOD, UNDEFINED = 0, (HEIGHT + 1) * (WIDTH + 1)
+    SNAKE = 2 * UNDEFINED
+
+    LEFT, RIGHT, UP, DOWN = -1, 1, -WIDTH, WIDTH
+
+    # 错误码
+    ERR = SNAKE
+
+    BG_COLOR = (255, 255, 255)
+    SNAKE_COLOR = (136, 0, 21)
+    FOOD_COLOR = (20, 220, 39)
 
 
-# 蛇类
-# 点以25为单位
 class Snake(object):
-    # 初始化各种需要的属性 [开始时默认向右/身体块x5]
-    def __init__(self):
-        self.dirction = pygame.K_RIGHT
-        self.body = []
-        for x in range(5):
-            self.addnode()
-        
-    # 无论何时 都在前端增加蛇块
-    def addnode(self):
-        left,top = (0,0)
-        if self.body:
-            left,top = (self.body[0].left,self.body[0].top)
-        node = pygame.Rect(left,top,25,25)
-        if self.dirction == pygame.K_LEFT:
-            node.left -= 25
-        elif self.dirction == pygame.K_RIGHT:
-            node.left += 25
-        elif self.dirction == pygame.K_UP:
-            node.top -= 25
-        elif self.dirction == pygame.K_DOWN:
-            node.top += 25
-        self.body.insert(0,node)
-        
-    # 删除最后一个块
-    def delnode(self):
-        self.body.pop()
-        
-    # 死亡判断
-    def isdead(self):
-        # 撞墙
-        if self.body[0].x not in range(SCREEN_X):
-            return True
-        if self.body[0].y not in range(SCREEN_Y):
-            return True
-        # 撞自己
-        if self.body[0] in self.body[1:]:
-            return True
-        return False
-        
-    # 移动！
-    def move(self):
-        self.addnode()
-        self.delnode()
-        
-    # 改变方向 但是左右、上下不能被逆向改变
-    def changedirection(self,curkey):
-        LR = [pygame.K_LEFT,pygame.K_RIGHT]
-        UD = [pygame.K_UP,pygame.K_DOWN]
-        if curkey in LR+UD:
-            if (curkey in LR) and (self.dirction in LR):
-                return
-            if (curkey in UD) and (self.dirction in UD):
-                return
-            self.dirction = curkey
-       
-       
-# 食物类
-# 方法： 放置/移除
-# 点以25为单位
-class Food:
-    def __init__(self):
-        self.rect = pygame.Rect(-25,0,25,25)
-        
-    def remove(self):
-        self.rect.x=-25
-    
-    def set(self):
-        if self.rect.x == -25:
-            allpos = []
-            # 不靠墙太近 25 ~ SCREEN_X-25 之间
-            for pos in range(25,SCREEN_X-25,25):
-                allpos.append(pos)
-            self.rect.left = random.choice(allpos)
-            self.rect.top  = random.choice(allpos)
-            print(self.rect)
-            
 
-def show_text(screen, pos, text, color, font_bold = False, font_size = 60, font_italic = False):   
-    #获取系统字体，并设置文字大小  
-    cur_font = pygame.font.SysFont("宋体", font_size)  
-    #设置是否加粗属性  
-    cur_font.set_bold(font_bold)  
-    #设置是否斜体属性  
-    cur_font.set_italic(font_italic)  
-    #设置文字内容  
-    text_fmt = cur_font.render(text, 1, color)  
-    #绘制文字  
-    screen.blit(text_fmt, pos)
+    def __init__(self):
 
-     
-def main():
-    pygame.init()
-    screen_size = (SCREEN_X,SCREEN_Y)
-    screen = pygame.display.set_mode(screen_size)
-    pygame.display.set_caption('Snake')
-    clock = pygame.time.Clock()
-    scores = 0
-    isdead = False
+        # 用一维数组来表示二维的东西
+        # board表示蛇运动的矩形场地
+        # 初始化蛇头在(1,1)的地方，第0行，HEIGHT行，第0列，WIDTH列为围墙，不可用
+        # 初始蛇长度为1
+        self.board = [0] * SnakeConf.FIELD_SIZE
+        self.snake = [0] * (SnakeConf.FIELD_SIZE + 1)
+        self.snake[0] = 1 * SnakeConf.WIDTH + 1
+        self.snake_size = 1
+
+        self.desperate = False
+
+        # 与上面变量对应的临时变量，蛇试探性地移动时使用
+        self.tmpboard = [0] * SnakeConf.FIELD_SIZE
+        self.tmpsnake = [0] * (SnakeConf.FIELD_SIZE + 1)
+        self.tmpsnake[0] = 1 * SnakeConf.WIDTH + 1
+        self.tmpsnake_size = 1
+
+        # food:食物位置(0~FIELD_SIZE-1),初始在(3, 3)
+        self.food = 3 * SnakeConf.WIDTH + 3
+
+        # 运动方向数组
+        self.mov = [SnakeConf.LEFT, SnakeConf.RIGHT, SnakeConf.UP, SnakeConf.DOWN]
+
+        self.desperate = False
+
+    def show_text(self, screen, pos, text, color, font_bold = False, font_size = 60, font_italic = False):
+        #获取系统字体，并设置文字大小
+        cur_font = pygame.font.SysFont("宋体", font_size)
+        #设置是否加粗属性
+        cur_font.set_bold(font_bold)
+        #设置是否斜体属性
+        cur_font.set_italic(font_italic)
+        #设置文字内容
+        text_fmt = cur_font.render(text, 1, color)
+        #绘制文字
+        screen.blit(text_fmt, pos)
+
+    def is_cell_free(self, idx, psize, psnake):
+        # 检查一个cell有没有被蛇身覆盖，没有覆盖则为free，返回true
+        return not (idx in psnake[:psize])
+
+    # 检查某个位置idx是否可向move方向运动
+    def is_move_possible(self, idx, move):
+        flag = False
+        if move == SnakeConf.LEFT:
+            flag = idx % SnakeConf.WIDTH > 1
+        elif move == SnakeConf.RIGHT:
+            flag = idx % SnakeConf.WIDTH < (SnakeConf.WIDTH - 2)
+        elif move == SnakeConf.UP:
+            flag = idx > (2 * SnakeConf.WIDTH - 1) # 即idx/WIDTH > 1
+        elif move == SnakeConf.DOWN:
+            flag = idx < (SnakeConf.FIELD_SIZE - 2 * SnakeConf.WIDTH) # 即idx/WIDTH < HEIGHT-2
+        return flag
+
+    # 重置board
+    # board_refresh后，UNDEFINED值都变为了到达食物的路径长度
+    # 如需要还原，则要重置它
+    def board_reset(self, psnake, psize, pboard):
+        for i in range(SnakeConf.FIELD_SIZE):
+            if i == self.food:
+                pboard[i] = SnakeConf.FOOD
+            elif self.is_cell_free(i, psize, psnake): # 该位置为空
+                pboard[i] = SnakeConf.UNDEFINED
+            else: # 该位置为蛇身
+                pboard[i] = SnakeConf.SNAKE
     
-    # 蛇/食物
-    snake = Snake()
-    food = Food()
-    
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                pressed_keys = pygame.key.get_pressed()
-                if pressed_keys.count(1) > 1:
-                    continue
-                snake.changedirection(event.key)
-                # 死后按space重新
-                if event.key == pygame.K_SPACE and isdead:
-                    return main()
-                
-            
-        screen.fill((255,255,255))
-        
-        # 画蛇身 / 每一步+1分
-        if not isdead:
-            scores+=1
-            snake.move()
-        for rect in snake.body:
-            pygame.draw.rect(screen,(20,220,39),rect,0)
-            
-        # 显示死亡文字
-        isdead = snake.isdead()
-        if isdead:
-            show_text(screen,(100,200),'YOU DEAD!',(227,29,18),False,100)
-            show_text(screen,(150,260),'press space to try again...',(0,0,22),False,30)
-            
-        # 食物处理 / 吃到+50分
-        # 当食物rect与蛇头重合,吃掉 -> Snake增加一个Node
-        if food.rect == snake.body[0]:
-            scores+=50
-            food.remove()
-            snake.addnode()
-        
-        # 食物投递
-        food.set()
-        pygame.draw.rect(screen,(136,0,21),food.rect,0)
-        
-        # 显示分数文字
-        show_text(screen,(50,500),'Scores: '+str(scores),(223,223,223))
-        
-        pygame.display.update()
-        clock.tick(10)
+    # 广度优先搜索遍历整个board，
+    # 计算出board中每个非SNAKE元素到达食物的路径长度
+    # 返回当前能否到达食物
+    def board_refresh(self, pfood, psnake, pboard):
+        queue = []
+        queue.append(pfood)
+        inqueue = [0] * SnakeConf.FIELD_SIZE #inque表示是否检查过这个点
+        found = False
+        # while循环结束后，除了蛇的身体，
+        # 其它每个方格中的数字代码从它到食物的路径长度
+        while len(queue) != 0:
+            idx = queue.pop(0)
+            if inqueue[idx] == 1:
+                continue
+            inqueue[idx] = 1
+            for move in self.mov:
+                if self.is_move_possible(idx, move):
+                    if idx + move == psnake[0]:
+                        found = True
+                    if pboard[idx+move] < SnakeConf.SNAKE: # 如果该点不是蛇的身体
+                        if pboard[idx + move] > pboard[idx] + 1:
+                            pboard[idx + move] = pboard[idx] + 1
+                        if inqueue[idx + move] == 0:
+                            queue.append(idx + move)
+
+        return found
+
+    # 从蛇头开始，根据board中元素值，
+    # 从蛇头周围4个领域点中选择最短路径
+    def choose_shortest_safe_move(self, psnake, pboard):
+        best_move = SnakeConf.ERR
+        min = SnakeConf.UNDEFINED
+        for move in self.mov:
+            if self.is_move_possible(psnake[0], move) \
+                    and pboard[psnake[0] + move] < min:
+                min = pboard[psnake[0] + move]
+                best_move = move
+        return best_move
+
+    # 从蛇头开始，根据board中元素值，
+    # 从蛇头周围4个领域点中选择最远路径
+    def choose_longest_safe_move(self, psnake, pboard):
+        best_move = SnakeConf.ERR
+        max = -1
+        for move in self.mov:
+            if self.is_move_possible(psnake[0], move) \
+                    and max < pboard[psnake[0] + move] < SnakeConf.UNDEFINED:
+                max = pboard[psnake[0] + move]
+                best_move = move
+        return best_move
+
+    # 检查是否可以追着蛇尾运动，即蛇头和蛇尾间是有路径的
+    # 为的是避免蛇头陷入死路
+    # 虚拟操作，在tmpboard,tmpsnake中进行
+    def is_tail_inside(self):
+        self.tmpboard[self.tmpsnake[self.tmpsnake_size - 1]] = SnakeConf.FOOD # 虚拟地将蛇尾变为食物(因为是虚拟的，所以在tmpsnake,tmpboard中进行)
+        self.tmpboard[self.food] = SnakeConf.SNAKE # 放置食物的地方，看成蛇身
+        result = self.board_refresh(self.tmpsnake[self.tmpsnake_size-1], self.tmpsnake, self.tmpboard) # 求得每个位置到蛇尾的路径长度
+        if not result: # 如果没有路直接返回
+            return False
+
+        if self.desperate:
+            result = False
+            for move in self.mov: # 如果蛇头和蛇尾紧挨着，则返回False。即不能follow_tail，追着蛇尾运动了
+                if self.is_move_possible(self.tmpsnake[0], move) \
+                        and self.tmpsnake[0] + move != self.tmpsnake[self.tmpsnake_size - 1]: # \
+                    result = True
+                else:
+                    self.tmpboard[self.tmpsnake[0] + move] = SnakeConf.UNDEFINED # 不符合
+        else: #蛇绝望前，走得比较保守，只要蛇头和蛇尾挨着，就不走
+            for move in self.mov: # 如果蛇头和蛇尾紧挨着，则返回False。即不能follow_tail，追着蛇尾运动了
+                if self.is_move_possible(self.tmpsnake[0], move) \
+                        and self.tmpsnake[0] + move == self.tmpsnake[self.tmpsnake_size - 1] \
+                        and self.tmpsnake_size > 3: # 如果不限制3的话，长度为1的蛇就不会再吃，seed(101)
+                    result = False
+
+        return result
+
+    # 让蛇头朝着蛇尾运行一步
+    # 不管蛇身阻挡，朝蛇尾方向运行
+    def follow_tail(self):
+        self.tmpsnake_size = self.snake_size
+        self.tmpsnake = self.snake[:]
+        self.board_reset(self.tmpsnake, self.tmpsnake_size, self.tmpboard) # 重置虚拟board
+        self.tmpboard[self.tmpsnake[self.tmpsnake_size - 1]] = SnakeConf.FOOD # 让蛇尾成为食物
+        self.tmpboard[self.food] = SnakeConf.SNAKE # 让食物的地方变成蛇身
+        self.board_refresh(self.tmpsnake[self.tmpsnake_size - 1], self.tmpsnake, self.tmpboard) # 求得各个位置到达蛇尾的路径长度
+        self.tmpboard[self.tmpsnake[self.tmpsnake_size - 1]] = SnakeConf.SNAKE # 还原蛇尾
+
+        return self.choose_longest_safe_move(self.tmpsnake, self.tmpboard) # 返回运行方向(让蛇头运动1步)
+
+    # 在各种方案都不行时，随便找一个可行的方向来走(1步),
+    def any_possible_move(self):
+        best_move = SnakeConf.ERR
+        self.board_reset(self.snake, self.snake_size, self.board)
+        self.board_refresh(self.food, self.snake, self.board)
+        min = SnakeConf.SNAKE
+
+        for move in self.mov:
+            if self.is_move_possible(self.snake[0], move) \
+                    and self.board[self.snake[0] + move] < min:
+                min = self.board[self.snake[0] + move]
+                best_move = move
+        return best_move
+
+    def shift_array(self, arr, size):
+        arr[1:size + 1] = arr[:size]
+
+    def new_food(self):
+        while True:
+            w = randint(1, SnakeConf.WIDTH-2)
+            h = randint(1, SnakeConf.HEIGHT-2)
+            food = h * SnakeConf.WIDTH + w
+            cell_free = self.is_cell_free(food, self.snake_size, self.snake)
+            if cell_free:
+                self.food = food
+                return
+
+    # 真正的蛇在这个函数中，朝pbest_move走1步
+    def make_move(self, pbest_move):
+        self.shift_array(self.snake, self.snake_size)
+        self.snake[0] += pbest_move
+
+        # 如果新加入的蛇头就是食物的位置
+        # 蛇长加1，产生新的食物，重置board(因为原来那些路径长度已经用不上了)
+        if self.snake[0] == self.food:
+            self.board[self.snake[0]] = SnakeConf.SNAKE # 新的蛇头
+            self.snake_size += 1
+            if self.snake_size < SnakeConf.FIELD_SIZE:
+                self.new_food()
+        else: # 如果新加入的蛇头不是食物的位置
+            self.board[self.snake[0]] = SnakeConf.SNAKE # 新的蛇头
+            self.board[self.snake[self.snake_size]] = SnakeConf.UNDEFINED # 蛇尾变为空格
+
+    # 虚拟地运行一次，然后在调用处检查这次运行可否可行
+    # 可行才真实运行。
+    # 虚拟运行吃到食物后，得到虚拟下蛇在board的位置
+    def virtual_shortest_move(self):
+        self.tmpsnake_size = self.snake_size
+        self.tmpsnake = self.snake[:] # 如果直接tmpsnake=snake，则两者指向同一处内存
+        self.tmpboard = self.board[:] # board中已经是各位置到达食物的路径长度了，不用再计算
+        self.board_reset(self.tmpsnake, self.tmpsnake_size, self.tmpboard)
+
+        food_eated = False
+        while not food_eated:
+            self.board_refresh(self.food, self.tmpsnake, self.tmpboard)
+            move = self.choose_shortest_safe_move(self.tmpsnake, self.tmpboard)
+            self.shift_array(self.tmpsnake, self.tmpsnake_size)
+            self.tmpsnake[0] += move # 在蛇头前加入一个新的位置
+            # 如果新加入的蛇头的位置正好是食物的位置
+            # 则长度加1，重置board，食物那个位置变为蛇的一部分(SNAKE)
+            if self.tmpsnake[0] == self.food:
+                self.tmpsnake_size += 1
+                self.board_reset(self.tmpsnake, self.tmpsnake_size, self.tmpboard) # 虚拟运行后，蛇在board的位置(label101010)
+                self.tmpboard[self.food] = SnakeConf.SNAKE
+                food_eated = True
+            else: # 如果蛇头不是食物的位置，则新加入的位置为蛇头，最后一个变为空格
+                self.tmpboard[self.tmpsnake[0]] = SnakeConf.SNAKE
+                self.tmpboard[self.tmpsnake[self.tmpsnake_size]] = SnakeConf.UNDEFINED
+
+    def draw_snake(self, screen):
+        left = (self.snake[0] // SnakeConf.WIDTH) * SnakeConf.LINE_WIDTH + 2 # margin 2
+        top = (self.snake[0] % SnakeConf.WIDTH) * SnakeConf.LINE_WIDTH + 2
+        rect = pygame.Rect(left, top, SnakeConf.LINE_TRUEWIDTH, SnakeConf.LINE_TRUEWIDTH)
+        pygame.draw.rect(screen, SnakeConf.SNAKE_COLOR, rect, 0)
+        for i in range(1, self.snake_size):
+            left_pre = (self.snake[i - 1] // SnakeConf.WIDTH) * SnakeConf.LINE_WIDTH + 2 # margin 2
+            top_pre = (self.snake[i - 1] % SnakeConf.WIDTH) * SnakeConf.LINE_WIDTH + 2
+            left = (self.snake[i] // SnakeConf.WIDTH) * SnakeConf.LINE_WIDTH + 2 # margin 2
+            top = (self.snake[i] % SnakeConf.WIDTH) * SnakeConf.LINE_WIDTH + 2
+            if (top_pre == top):
+                rect = pygame.Rect(min(left, left_pre), top, SnakeConf.LINE_TRUEWIDTH + SnakeConf.LINE_WIDTH, SnakeConf.LINE_TRUEWIDTH)
+            else:
+                rect = pygame.Rect(left, min(top, top_pre), SnakeConf.LINE_TRUEWIDTH, SnakeConf.LINE_TRUEWIDTH + SnakeConf.LINE_WIDTH)
+            pygame.draw.rect(screen, SnakeConf.SNAKE_COLOR, rect, 0)
+
+    def main(self):
+        pygame.init()
+        screen = pygame.display.set_mode((SnakeConf.SCREEN_X, SnakeConf.SCREEN_Y))
+        pygame.display.set_caption('Snake')
+        clock = pygame.time.Clock()
+
+        isdead = False
+
+        dead_loop = 0
+        pre_score = 0
+
+        pause = False
+        onestep = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    pause = not pause
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                    onestep = True
+
+            if pause and not onestep:
+                continue
+
+            onestep = False
+
+            screen.fill(SnakeConf.BG_COLOR)
+
+            self.draw_snake(screen)
+
+            rect = pygame.Rect((self.food//SnakeConf.WIDTH)*SnakeConf.LINE_WIDTH + 2,(self.food%SnakeConf.WIDTH)*SnakeConf.LINE_WIDTH +2 ,SnakeConf.LINE_TRUEWIDTH, SnakeConf.LINE_TRUEWIDTH)
+            pygame.draw.rect(screen,SnakeConf.FOOD_COLOR,rect,0)
+
+            # 重置矩阵
+            self.board_reset(self.snake, self.snake_size, self.board)
+
+            # 如果蛇可以吃到食物，board_refresh返回true
+            # 并且board中除了蛇身(=SNAKE)，其它的元素值表示从该点运动到食物的最短路径长
+            if self.board_refresh(self.food, self.snake, self.board):
+                # 虚拟地运行一次，因为已经确保蛇与食物间有路径，所以执行有效
+                # 运行后得到虚拟下蛇在board中的位置，即tmpboard
+                self.virtual_shortest_move()  # 该函数唯一调用处，在虚拟的board
+                if self.is_tail_inside():  # 如果虚拟运行后，蛇头蛇尾间有通路，则选最短路运行(1步)
+                    best_move = self.choose_shortest_safe_move(self.snake, self.board)
+                else:
+                    best_move = self.follow_tail()  # 否则虚拟地follow_tail 1步，如果可以做到
+            else:
+                best_move = self.follow_tail()
+
+            if best_move == SnakeConf.ERR:
+                best_move = self.any_possible_move()
+            # 上面一次思考，只得出一个方向，运行一步
+            if best_move != SnakeConf.ERR:
+                self.make_move(best_move)
+            else:
+                isdead = True
+
+            if isdead:
+                self.show_text(screen,(100,200),'YOU DEAD!',(227,29,18), False, 100)
+                self.show_text(screen,(150,260),'press space to try again...',(0,0,22), False, 30)
+
+            # 显示分数文字
+            self.show_text(screen,(50,500),'Scores: '+str(self.snake_size),(223,223,223))
+
+            pygame.display.update()
+            if (self.snake_size == pre_score):
+                dead_loop += 1
+            else:
+                dead_loop = 0
+            if dead_loop > SnakeConf.FIELD_SIZE:
+                self.desperate = True # 蛇绝望了
+                print('desperate')
+            pre_score = self.snake_size
+
+            clock.tick(100) # 这里调的是帧律
     
     
 if __name__ == '__main__':
-    main()
+    snake1 = Snake()
+    snake1.main()
